8c61f8f180c6f7aa15582f07ff598cd95d9bdafc
dieghomj <diegomartj03@gmail.com>
2023-12-03 17:57:46 -0500

    Added Binder template

---
+++ Gsharp/Code Analysis/Binder.cs
@@ -0,0 +1,12 @@
+class Binder
+{
+    private Dictionary<string, GType> visibleVariables = new Dictionary<string, GType>();
+    public Binder(Expression root)
+    {
+        Root = root;
+    }
+
+    public Expression Root { get; }
+
+    public BoundExpression Bind() => Root.GetBoundExpression(visibleVariables);
+}
