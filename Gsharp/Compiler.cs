f268e70bdc0301a7354a254e732d7b911e8cfa6d
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2023-12-07 16:18:43 -0500

    Implement Compile

--- Gsharp/Compiler.cs
+++ Gsharp/Compiler.cs
@@ -3,7 +3,7 @@ using System.Timers;
 
 namespace Gsharp;
 
-public class Compiler
+public static class Compiler
 {
     private static readonly List<(Figure figure, Color color, string message)> figures =
         new List<(Figure, Color, string)>();
@@ -11,9 +11,53 @@ public class Compiler
     private static bool isModified = false;
     private static readonly System.Timers.Timer timer = new System.Timers.Timer();
 
-    public Compiler(string code) { }
+    private static List<Statement> syntaxStatements = new List<Statement>();
+    private static List<BoundStatement> boundStatements = new List<BoundStatement>();
+
+    public static void Compile(string code)
+    {
+        Reset();
+
+        GetSyntaxStatements(code);
+
+        Dictionary<string, GType> visibleVariables = new Dictionary<string, GType>();
+
+        foreach (Statement statement in syntaxStatements)
+            AddBoundStatement(statement.GetBoundStatement(visibleVariables));
+    }
 
     public static void Evaluate() { }
+
+    public static void Reset()
+    {
+        syntaxStatements = new List<Statement>();
+        boundStatements = new List<BoundStatement>();
+    }
+
+    private static void GetSyntaxStatements(string code)
+    {
+        Parser parser = new Parser(code);
+
+        List<ImportStatement> importStatements = parser.GetImportStatements();
+
+        foreach (ImportStatement import in importStatements)
+        {
+            BoundImportStatement boundImport = (BoundImportStatement)
+                import.GetBoundStatement(new Dictionary<string, GType>());
+
+            boundImport.EvaluateStatement(new Dictionary<string, GObject>());
+        }
+
+        List<Statement> statements = parser.Parse();
+
+        foreach (Statement statement in statements)
+            AddSyntaxStatement(statement);
+    }
+
+    public static void AddSyntaxStatement(Statement statement) => syntaxStatements.Add(statement);
+
+    private static void AddBoundStatement(BoundStatement boundStatement) =>
+        boundStatements.Add(boundStatement);
 
     public static void AddFigure(Figure figure, string message)
     {
