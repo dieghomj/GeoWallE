d30e468838991ee098f521033ce623e1690012ca
Ernesto Abreu Peraza <ernestoabreup@gmail.com>
2023-12-07 11:50:53 -0500

    Fix Equals and NotEquals Evaluate

--- Gsharp/GObject/Number.cs
+++ Gsharp/GObject/Number.cs
@@ -74,6 +74,22 @@ public class Number : GObject
             return new Number(0);
     }
 
+    public static Number operator ==(Number a, Number b)
+    {
+        if (a.Value == b.Value)
+            return new Number(1);
+        else
+            return new Number(0);
+    }
+
+    public static Number operator !=(Number a, Number b)
+    {
+        if (a.Value != b.Value)
+            return new Number(1);
+        else
+            return new Number(0);
+    }
+
     #endregion
 
     #region Unary Operators
@@ -91,4 +107,24 @@ public class Number : GObject
     }
 
     #endregion
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        if (ReferenceEquals(obj, null))
+        {
+            return false;
+        }
+
+        throw new NotImplementedException();
+    }
+
+    public override int GetHashCode()
+    {
+        throw new NotImplementedException();
+    }
 }
